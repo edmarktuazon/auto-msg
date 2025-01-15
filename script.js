b568ae2db2f39b77013b8ec2831e9aa7c4ab4d8a
edmarktuazon <edmarktuazon03@gmail.com>
2025-01-15 16:52:42 +0800

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       buttonId: "reviewBtn",
       containerId: "reviewComment",
-      message: `Hi, \n\nThe Templates are ready for review.\n\nAttached is the version 1 of the proof file:\n\nPlease let me know if you have any requests or need content adjustments.\n\nThanks,\nEd`,
+      message: `Hi, \n\nThe Templates are ready for review. Attached is the version 1 of the proof file:\n\nPlease let me know if you have any requests or need content adjustments.\n\nThanks,\nEd`,
     },
     {
       buttonId: "templateIDBtn",
@@ -18,12 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const textarea = document.createElement("textarea");
     const copyButton = document.createElement("button");
+    // const randomButton = document.createElement("button");
     copyButton.textContent = "Copy text";
     copyButton.style.display = "none";
+
+    // randomButton.textContent = "Random comment";
+    // randomButton.style.display = "none";
     textarea.style.display = "none";
 
     container.appendChild(textarea);
     container.appendChild(copyButton);
+    // container.appendChild(randomButton);
 
     const generateMessage = () => {
       textarea.value = section.message;
@@ -31,7 +36,42 @@ document.addEventListener("DOMContentLoaded", () => {
       textarea.classList.add("review-area");
       copyButton.style.display = "inline-block";
       copyButton.classList.add("copy-button");
+      randomButton.style.display = "inline-block";
+      // randomButton.classList.add("random-button");
     };
+
+
+    const generateRandomButton = () => {
+      // generate random comments 
+      const randomCommentVal = "Text"
+
+      const randomComment = document.querySelector("#randomComment");
+      const randomCommentEl = document.createElement("p");
+      
+      randomCommentEl.innerHTML = randomCommentVal;
+      // localStorage.setItem(section.message, "Test");
+
+
+      // if(section.message === true){
+      //   console.log("true");
+      // }
+
+      // for(let i = 0; i > 1; i++) {
+      //   if(section.message = 1) {
+      //     console.log(true, "Comment has been created");
+      //     // randomComment.appendChild(randomCommentEl);
+      //     console.log(section.message);
+      //   }else {
+      //     console.log(false, "Comment has not been created");
+      //     console.log(section.message);
+
+      //   }
+      // }
+      
+      // randomComment.appendChild(randomCommentEl);
+
+    }
+         
 
     const copyMessage = () => {
       navigator.clipboard.writeText(section.message);
@@ -39,5 +79,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     button.addEventListener("click", generateMessage);
     copyButton.addEventListener("click", copyMessage);
+    // randomButton.addEventListener("click",  generateRandomButton);
   });
 });
